15077d7e49ebb60aeca54cad5773393dd660f12d
jkohl7 <jkohl7@my.wcyc.edu>
2020-02-12 13:17:14 -0600

    Now shows the top 10 words that are used. Also, the words that are only used once are displayed.

--- src/edu/wctc/Main.java
+++ src/edu/wctc/Main.java
@@ -39,9 +39,31 @@ public class Main {
             }
         }
 
+        System.out.println("The top 10 most used words.");
+        System.out.println("---------------------------");
+
         // Loop over entries in the map, getting each key/value pair
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
+            // System.out.println(entry.getKey() + " " + entry.getValue());
+
+            if (entry.getValue() > 600)
+                System.out.println(entry.getKey() + " " + entry.getValue());
+
+        }
+        System.out.println("__________________________________");
+
+
+
+        System.out.println("Words that are only used once.");
+        System.out.println("----------------------------");
+
+        // Loop over entries in the map, getting each key/value pair.
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            // System.out.println(entry.getKey() + " " + entry.getValue());
+
+            if (entry.getValue().equals(1))
+                System.out.println(entry.getKey());
+
         }
     }
 
